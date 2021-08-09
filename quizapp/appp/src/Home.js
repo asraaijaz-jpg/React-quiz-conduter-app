b1ea75a2992e998582f73f8f9a0e7d2bef111619
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2021-08-09 10:31:31 +0500

    commit

--- quizapp/appp/src/Home.js
+++ quizapp/appp/src/Home.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 function Home ()
 {
-    return(<h1>I am  home come through routing hello</h1>);
+    return(<h1>I am  home come through routing hello and I commmit code and i update in githb</h1>);
 }
 export default Home;
